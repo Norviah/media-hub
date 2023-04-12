46550791b04aa8006af120b1d60dcf8778e4b972
norviah <joseph>
2023-04-11 17:38:35 -0700

    feat: setup next and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Component } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+
+export default class App extends Component<AppProps> {
+  public render(): JSX.Element {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { Component } from "react";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class Document extends Component {
+  public render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
