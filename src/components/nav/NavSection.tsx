7ee20cf858c1fa90a8ffdca9be89f72428b4bcc6
norviah <joseph>
2023-04-13 16:24:49 -0700

    style(frontend/components/nav): wrap the routes in a stack for padding

--- src/components/nav/NavSection.tsx
+++ src/components/nav/NavSection.tsx
@@ -1,24 +1,14 @@
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItemText from '@mui/material/ListItemText';
+import Stack from '@mui/material/Stack';
 
 import { Link } from '@/components/Link';
 import { StyledNavItem, StyledNavItemIcon } from '@/components/nav/styles';
 import { routes } from '@/util/routes';
 
 import type { Route } from '@/types/Route';
-
-export default function NavSection({ data = [], ...other }) {
-  return (
-    <Box {...other}>
-      <List disablePadding sx={{ p: 1 }}>
-        {routes.map((item: Route) => (
-          <NavItem key={item.name} item={item} />
-        ))}
-      </List>
-    </Box>
-  );
-}
+import type { BoxProps } from '@mui/material';
 
 function NavItem(props: { item: Route }) {
   return (
@@ -36,5 +26,19 @@ function NavItem(props: { item: Route }) {
       <StyledNavItemIcon>{props.item.icon && <props.item.icon />}</StyledNavItemIcon>
       <ListItemText disableTypography primary={props.item.name} />
     </StyledNavItem>
+  );
+}
+
+export default function NavSection(props: { data: Route[]; other?: BoxProps }): JSX.Element {
+  return (
+    <Box {...props?.other}>
+      <List sx={{ p: 1 }}>
+        <Stack spacing={1}>
+          {routes.map((item: Route) => (
+            <NavItem key={item.name} item={item} />
+          ))}
+        </Stack>
+      </List>
+    </Box>
   );
 }
