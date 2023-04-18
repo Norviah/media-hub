01ab839d37d582a4d3066e93b064710e1ec04606
norviah <joseph>
2023-04-17 18:30:57 -0700

    chore(frontend/layout/auth): edit the auth/validation errors

--- src/layout/auth/util/AuthErrors.ts
+++ src/layout/auth/util/AuthErrors.ts
@@ -1,6 +1,10 @@
+/**
+ * The various errors that can be thrown from next-auth.
+ *
+ * @see https://next-auth.js.org/configuration/pages#sign-in-page
+ */
 export const AuthErrors = {
-  OAuthAccountNotLinked:
-    'Please use the original method you used to create your account to sign in.',
+  OAuthAccountNotLinked: 'Please log in using the original method you used to create your account.',
   CredentialsSignin: "We couldn't find an account with those credentials, please try again.",
 };
 
