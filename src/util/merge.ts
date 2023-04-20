6ce5c386aa1c4beae22639d3625cd84d77fad375
norviah <joseph>
2023-04-20 01:40:46 -0700

    refactor(util/merge): implement a third-party module for merging objects

--- src/util/merge.ts
+++ src/util/merge.ts
@@ -1,30 +1,3 @@
-/**
- * Merges two objects together.
- *
- * Given two objects, `merge` will initialize a new object consisting of
- * properties from both objects. `overrides` will act as a source of truth
- * for properties that exist in both objects, and will override the values
- * of the `source` object.
- * @template T The type of the objects.
- * @param source The source object.
- * @param overrides Values to override in the resulting object.
- */
-export function merge<T>(source: T, overrides: Partial<T>): T {
-  const object: Record<string, any> = {};
+import merge from 'ts-deepmerge';
 
-  for (const key in source) {
-    if (typeof source[key] === 'object' && typeof overrides[key] === 'object') {
-      object[key] = merge(source[key], overrides[key]);
-    } else {
-      object[key] = source[key];
-    }
-  }
-
-  for (const key in overrides) {
-    if (typeof source[key] === 'undefined') {
-      object[key] = overrides[key];
-    }
-  }
-
-  return object as T;
-}
+export { merge };
