05b487c7177d1dfca3f40096fe1218ab053a797e
norviah <joseph>
2023-04-19 20:50:41 -0700

    refactor(frontend/components/label): clean up component from the template

--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,46 +1,83 @@
-import { forwardRef } from 'react';
-// @mui
+import Box from '@mui/material/Box';
+import IconStyle from './styles/icon';
+import LayoutContainer from './layout/LabelContainer';
+
 import { useTheme } from '@mui/material/styles';
-import { Box } from '@mui/material';
-//
-import { StyledLabel } from './styles';
 
-// ----------------------------------------------------------------------
+import type { LabelProps } from '@/types/components/label';
 
-// const Label = forwardRef(
-//   ({ children, color = 'default', variant = 'soft', startIcon, endIcon, sx, ...other }, ref) => {
-function Label(props: any) {
+/**
+ * Creates a small box within a container for labeling purposes.
+ *
+ * `Label` is a simple UI component that can be used to add a label to any
+ * container. The component creates a small box within the specified container,
+ * the location of which can be defined using the `spacing` prop.
+ * @param props
+ * @returns
+ * @example
+ * ```tsx
+ * import Box from '@mui/material/Box';
+ * import Card from '@mui/material/Card';
+ * import CardContent from '@mui/material/CardContent';
+ * import CardMedia from '@mui/material/CardMedia';
+ * import Typography from '@mui/material/Typography';
+ *
+ * import { Label } from '@/components/label';
+ *
+ * export default function Page(): JSX.Element {
+ *   return (
+ *     <Box sx={{ p: 2 }}>
+ *       <Card sx={{ width: 350 }}>
+ *         <Label
+ *           color="info"
+ *           variant="filled"
+ *           text="Sample Text"
+ *           spacing={{
+ *             top: 16,
+ *             right: 16,
+ *           }}
+ *         />
+ *         <CardMedia
+ *           sx={{ height: 200 }}
+ *           image="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/IGbeFv5Ji4W0x530JcSHiQpamY.jpg"
+ *           title="green iguana"
+ *         />
+ *         <CardContent>
+ *           <Typography gutterBottom variant="h5" component="div">
+ *             Title
+ *           </Typography>
+ *           <Typography variant="body2" color="text.secondary">
+ *             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ *             Nulla non purus risus. Sed ut dignissim erat. Nam consequat
+ *             ullamcorper consequat.
+ *           </Typography>
+ *         </CardContent>
+ *       </Card>
+ *     </Box>
+ *   );
+ * }
+ * ```
+ *
+ * In this example, we are creating a card and defining a `Label` component to
+ * be rendered within the top-right corner of the card.
+ */
+export default function Label(props: LabelProps): JSX.Element {
   const theme = useTheme();
 
-  const { ref, color, variant, startIcon, endIcon, sx, other, children } = props;
-
-  const iconStyle = {
-    width: 16,
-    height: 16,
-    '& svg, img': { width: 1, height: 1, objectFit: 'cover' },
-  };
-
   return (
-    <StyledLabel
-      ref={ref}
+    <LayoutContainer
       component="span"
-      ownerState={{ color, variant }}
       sx={{
-        ...(startIcon && { pl: 0.75 }),
-        ...(endIcon && { pr: 0.75 }),
-        ...sx,
+        ...(props.icons?.start && { pl: 0.75 }),
+        ...(props.icons?.end && { pr: 0.75 }),
+        ...props.sx,
       }}
       theme={theme}
-      {...other}
+      {...props}
     >
-      {startIcon && <Box sx={{ mr: 0.75, ...iconStyle }}> {startIcon} </Box>}
-
-      {children}
-
-      {endIcon && <Box sx={{ ml: 0.75, ...iconStyle }}> {endIcon} </Box>}
-    </StyledLabel>
+      {props.icons?.start && <Box sx={{ mr: 0.75, ...IconStyle }}>{<props.icons.start />}</Box>}
+      {typeof props.data === 'string' ? props.data : <Box sx={IconStyle}>{<props.data />}</Box>}
+      {props.icons?.end && <Box sx={{ ml: 0.75, ...IconStyle }}>{<props.icons.end />}</Box>}
+    </LayoutContainer>
   );
 }
-// );
-
-export default Label;
---
+++ src/components/label/styles/icon.ts
@@ -0,0 +1,11 @@
+const icon = {
+  width: 16,
+  height: 16,
+  '& svg, img': {
+    width: 1,
+    height: 1,
+    objectFit: 'cover',
+  },
+};
+
+export default icon;
