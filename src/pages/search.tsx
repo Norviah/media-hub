d98751e7986780e7ff55a0a4cef7a6d98f051650
norviah <joseph>
2023-04-19 18:20:12 -0700

    refactor(frontend/pages/search): import mui components directly from their respective file

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 import { useState } from 'react';
-import { Container, Stack, Typography } from '@mui/material';
 import { ProductSort, ProductList, ProductFilterSidebar } from '@/layout/products';
 import PRODUCTS from '@/components/_mock/products';
 
-// ----------------------------------------------------------------------
-
-export default function ProductsPage() {
+export default function ProductsPage(): JSX.Element {
   const [openFilter, setOpenFilter] = useState(false);
 
   const handleOpenFilter = () => {
