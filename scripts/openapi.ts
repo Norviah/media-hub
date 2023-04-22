662fba1221a18d2444184a99b345ebd2ac7c9a4d
norviah <joseph>
2023-04-21 21:39:02 -0700

    feat(backend): implement the script to generate openapi specifications

---
+++ scripts/openapi.ts
@@ -0,0 +1,36 @@
+import { join } from 'path';
+import { writeFileSync } from 'fs';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+import type { Options } from 'swagger-jsdoc';
+
+import * as paths from '@/util/paths';
+
+const options: Options = {
+  failOnErrors: true,
+
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Watch Together',
+      version: '0.1.0',
+      license: {
+        name: 'MIT',
+        url: 'https://raw.githubusercontent.com/Norviah/watch-together/master/LICENSE',
+      },
+    },
+    'x-tagGroups': [
+      {
+        name: 'User Management',
+        tags: ['user'],
+      },
+    ],
+    tags: [{ name: 'user', description: 'Operations related to a user.' }],
+  },
+
+  apis: [join(paths.CONTROLLERS, '**/*.ts')],
+};
+
+writeFileSync(paths.OPENAPI, JSON.stringify(swaggerJsdoc(options), null, 2));
+
+console.log('[OPENAPI] generated openapi specifications');
