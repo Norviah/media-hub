82013b31be511fe778a5d94b16f9966d9e2fc160
norviah <21983700+Norviah@users.noreply.github.com>
2023-11-08 20:27:18 -0800

    feat: implement a menu to represent the user

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,20 @@
 import { CommandMenu } from '@/components/CommandMenu';
 import { ThemeSelector } from '@/components/ThemeSelector';
+import { SpinnerIcon } from '@/components/icons/Spinner';
 import { Header } from '@/components/layout/Header';
+import { UserMenu } from '@/components/layout/UserMenu';
 import { NavBar } from '@/components/layout/navigation/NavBar';
+import { Suspense } from 'react';
+
+import { getCurrentUser } from '@/utils/auth/session';
 
 import type { LayoutProps } from '@/types/components/LayoutProps';
+
+async function RenderUserMenu(): Promise<JSX.Element> {
+  const user = await getCurrentUser();
+
+  return <UserMenu user={user} />;
+}
 
 export default function DashboardLayout(props: LayoutProps): JSX.Element {
   return (
@@ -13,6 +24,15 @@ export default function DashboardLayout(props: LayoutProps): JSX.Element {
         <div className="flex gap-1">
           <CommandMenu />
           <ThemeSelector />
+          <Suspense
+            fallback={
+              <span className="inline-flex h-9 items-center justify-center rounded-md text-sm font-medium text-muted-foreground transition-colors">
+                <SpinnerIcon className="h-5 w-5 animate-spin text-foreground" />
+              </span>
+            }
+          >
+            <RenderUserMenu />
+          </Suspense>
         </div>
       </Header>
 
