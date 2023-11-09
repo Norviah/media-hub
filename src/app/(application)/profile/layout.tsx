5c9807fe548ce91f3f4a11338b3c017b4e4fb3a1
norviah <21983700+Norviah@users.noreply.github.com>
2023-11-09 02:31:23 -0800

    style(layout/Profile): implement a sidebar for the profile route

---
+++ src/app/(application)/profile/layout.tsx
@@ -0,0 +1,13 @@
+import { ProfileSidebar } from '@/components/profile/ProfileSidebar';
+import { LayoutProps } from '@/types/components/LayoutProps';
+
+export default function ProfileLayout(props: LayoutProps): JSX.Element {
+  return (
+    <div className="grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+      <aside className="hidden w-[200px] flex-col md:flex">
+        <ProfileSidebar />
+      </aside>
+      <main className="flex w-full flex-1 flex-col overflow-hidden">{props.children}</main>
+    </div>
+  );
+}
