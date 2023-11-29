2d9f956c50450fe8ad201953cd1d0a6bfaefff65
norviah <21983700+Norviah@users.noreply.github.com>
2023-11-29 13:19:21 -0800

    style: implement the 'brandon grotesque' font

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-import { League_Spartan as LeagueSpartan } from 'next/font/google';
+import localFont from 'next/font/local';
 import type { Metadata } from 'next';
 
 export const site = {
@@ -23,7 +23,67 @@ export const site = {
   } satisfies Metadata,
 };
 
-export const font = LeagueSpartan({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+export const font = localFont({
+  src: [
+    {
+      path: '../../public/fonts/BrandonGrotesque-Black.otf',
+      style: 'normal',
+      weight: '900',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-BlackItalic.otf',
+      style: 'italic',
+      weight: '900',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-Bold.otf',
+      style: 'normal',
+      weight: '700',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-BoldItalic.otf',
+      style: 'italic',
+      weight: '700',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-Light.otf',
+      style: 'normal',
+      weight: '300',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-LightItalic.otf',
+      style: 'italic',
+      weight: '300',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-Medium.otf',
+      style: 'normal',
+      weight: '500',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-MediumItalic.otf',
+      style: 'italic',
+      weight: '500',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-Regular.otf',
+      style: 'normal',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-RegularItalic.otf',
+      style: 'italic',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-Thin.otf',
+      style: 'normal',
+      weight: '100',
+    },
+    {
+      path: '../../public/fonts/BrandonGrotesque-ThinItalic.otf',
+      style: 'italic',
+      weight: '100',
+    },
+  ],
 });
