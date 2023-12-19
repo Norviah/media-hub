6216fc6b5bafa8d34682bbbe0b43f89f9256f970
norviah <21983700+Norviah@users.noreply.github.com>
2023-12-19 04:45:15 -0800

    style(page/search): remove x padding

--- src/app/(application)/search/layout.tsx
+++ src/app/(application)/search/layout.tsx
@@ -3,7 +3,7 @@ import { FilterList } from '@/components/search/filter/FilterList';
 
 export default function SearchLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
-    <div className="relative mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 md:flex-row">
+    <div className="relative mx-auto flex max-w-screen-2xl flex-col gap-8 pb-4 md:flex-row">
       <div className="order-last min-h-screen w-full md:order-none">{children}</div>
       <div className="order-none flex flex-col gap-6 md:order-last md:w-[125px]" style={{ position: 'sticky', top: 0 }}>
         <FilterList list={filters} title="Filter by" />
