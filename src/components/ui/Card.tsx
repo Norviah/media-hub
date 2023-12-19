3c8e31c7052d4b3bf60a6c5cff0c697a03ac0ffd
norviah <21983700+Norviah@users.noreply.github.com>
2023-12-19 01:23:30 -0800

    style(components/Card): remove border for all cards

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/utils/cn';
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('rounded-lg border bg-card text-card-foreground shadow-md transition-shadow hover:shadow-lg', className)}
+    className={cn('rounded-lg bg-card text-card-foreground shadow-md transition-shadow hover:shadow-lg', className)}
     {...props}
   />
 ));
