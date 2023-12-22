a516888c6803bd8d0a2083a1437a48664dd5d3da
Norviah <21983700+Norviah@users.noreply.github.com>
2023-12-22 03:22:13 -0800

    chore(page/search): change the title for filters to `Search for`

--- src/app/(application)/search/layout.tsx
+++ src/app/(application)/search/layout.tsx
@@ -6,7 +6,7 @@ export default function SearchLayout({ children }: { children: React.ReactNode }
     <div className="relative mx-auto flex max-w-screen-2xl flex-col gap-8 pb-4 md:flex-row">
       <div className="order-last min-h-screen w-full md:order-none">{children}</div>
       <div className="order-none flex flex-col gap-6 md:order-last md:w-[125px]" style={{ position: 'sticky', top: 0 }}>
-        <FilterList list={filters} title="Filter by" />
+        <FilterList list={filters} title="Search for" />
         <FilterList list={layouts} title="View by" />
       </div>
     </div>
