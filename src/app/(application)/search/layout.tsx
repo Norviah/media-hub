f2d3c5a8ba3723877d8b93f45ebc585dc18bcce6
Norviah <21983700+Norviah@users.noreply.github.com>
2023-12-29 07:51:40 -0800

    fix(page/search): ensure ssr by wrapping all children in a suspense boundary

--- src/app/(application)/search/layout.tsx
+++ src/app/(application)/search/layout.tsx
@@ -1,11 +1,13 @@
 import { SearchForm } from '@/systems/search/components/filters/SearchForm';
+import { Suspense } from 'react';
+
 import type { LayoutProps } from '@/types/components/LayoutProps';
 
 export default function SearchPage({ children }: LayoutProps): JSX.Element {
   return (
-    <>
+    <Suspense>
       <SearchForm />
       {children}
-    </>
+    </Suspense>
   );
 }
