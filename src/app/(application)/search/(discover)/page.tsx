4cdde79c3619472124714f9256bc6e9016dc6128
Norviah <21983700+Norviah@users.noreply.github.com>
2023-12-30 02:48:34 -0800

    style(page/search): change heading and spacing for trending page

--- src/app/(application)/search/(discover)/page.tsx
+++ src/app/(application)/search/(discover)/page.tsx
@@ -6,16 +6,16 @@ import { Suspense } from 'react';
 
 export default function SearchPage(): JSX.Element {
   return (
-    <div className="flex flex-col gap-5">
-      <div className="flex flex-col gap-3">
-        <Header type="h3">Trending Movies</Header>
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-5">
+        <Header type="h4">Trending Movies</Header>
         <Suspense fallback={<RowSkeleton />}>
           <TrendingMovies />
         </Suspense>
       </div>
 
-      <div className="flex flex-col gap-3">
-        <Header type="h3">Trending TV Shows</Header>
+      <div className="flex flex-col gap-5">
+        <Header type="h4">Trending TV Shows</Header>
         <Suspense fallback={<RowSkeleton />}>
           <TrendingTVShows />
         </Suspense>
