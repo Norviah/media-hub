08a62c1807aa5b03ede900520365772a558b18a7
Norviah <21983700+Norviah@users.noreply.github.com>
2023-12-31 23:10:42 -0800

    feat(systems/search): keep similar genres when switching from tv shows to movies and vice-versa

--- src/systems/search/components/filters/Format.tsx
+++ src/systems/search/components/filters/Format.tsx
@@ -8,7 +8,10 @@ import { SearchParams } from '@/utils/params';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { basePath, paths } from '../../util/constants';
 import { constructUrl } from '../../util/constructUrl';
+import { movieGenres, tvGenres } from '../../util/genres';
+import { parseParams } from '../../util/parseParams';
 
+import type { Genre } from '@/actions/tmdb';
 import type { Route } from 'next';
 
 export function Format(): JSX.Element {
@@ -23,12 +26,23 @@ export function Format(): JSX.Element {
       value={currentPath.path}
       onValueChange={(value: Route) => {
         if (value !== currentPath.path) {
+          const { genres: parsedGenres } = parseParams(pathname, params);
+
+          let genres: Genre[] | undefined;
+
+          if (pathname === basePath.path) {
+            genres = undefined;
+          } else {
+            const toGenres = pathname.includes('tv') ? movieGenres : tvGenres;
+            genres = parsedGenres?.filter((genre) => toGenres.some((g) => g.id === genre.id));
+          }
+
           router.push(
             constructUrl({
               path: value,
               params,
               reset: value === basePath.path,
-              overrides: { [SearchParams.GENRES]: undefined },
+              overrides: { [SearchParams.GENRES]: genres?.map((genre) => genre.name.toLowerCase()) },
             })
           );
         }
