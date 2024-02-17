22d8c702970452a9183fb4b41acb30ec212fe17e
Norviah <21983700+Norviah@users.noreply.github.com>
2024-02-16 18:23:48 -0800

    style(components): update shadcn components styling to work with lighter background for light mode

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/utils/cn';
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn('text-card-foreground rounded-lg bg-card shadow-md transition-shadow hover:shadow-lg', className)}
+    className={cn('text-card-foreground rounded-lg border bg-card shadow-md transition-shadow hover:shadow-lg', className)}
     {...props}
   />
 ));
@@ -18,22 +18,18 @@ CardHeader.displayName = 'CardHeader';
 
 const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
   ({ className, ...props }, ref) => (
-    <h3
-      ref={ref}
-      className={cn('text-xl font-semibold leading-none tracking-tight text-foreground', className)}
-      {...props}
-    />
+    <h3 ref={ref} className={cn('font-semibold leading-none tracking-tight', className)} {...props} />
   )
 );
 CardTitle.displayName = 'CardTitle';
 
 const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
-  ({ className, ...props }, ref) => <p ref={ref} className={cn('text-sm text-card-muted', className)} {...props} />
+  ({ className, ...props }, ref) => <p ref={ref} className={cn('text-sm text-muted-foreground', className)} {...props} />
 );
 CardDescription.displayName = 'CardDescription';
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
+  ({ className, ...props }, ref) => <div ref={ref} className={cn('text-card-foreground p-6 pt-0', className)} {...props} />
 );
 CardContent.displayName = 'CardContent';
 
