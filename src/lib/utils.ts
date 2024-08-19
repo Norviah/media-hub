2b6f8530b7d5ac5f145f01902debb22200337f6d
Norviah <21983700+Norviah@users.noreply.github.com>
2024-08-19 13:10:32 -0700

    feat(utils): implement a helper function to construct urls

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,9 @@
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+import type { ConstrainedRecord } from '@/types';
 import type { ClassValue } from 'clsx';
+import type { Route } from 'next';
 
 /**
  * A utility function that is a wrapper around the the `clsx` and
@@ -32,4 +34,151 @@ export function cn(...classes: ClassValue[]) {
  */
 export function capitalize(string: string): string {
   return string.replace(/\b\w/g, (l) => l.toUpperCase());
+}
+
+/**
+ * @template T The structure of query parameters.
+ * @template Path The path for which to construct the URL.
+ */
+export type ConstructUrlOptions<T extends ConstrainedRecord<T>, Path extends string> = {
+  /**
+   * The route to construct a URL for.
+   *
+   * This property defines the specific route within the application for which
+   * the URL will be constructed. It should represent a valid route or endpoint
+   * in the application, and the resulting URL will be based on this path.
+   *
+   * @example
+   *
+   * ```ts
+   * "/search"
+   * ```
+   */
+  route: Route<Path>;
+
+  /**
+   * The current query parameters present in the URL.
+   *
+   * These parameters represent the key-value pairs that are extracted from the
+   * URL, they indicate the current state of the page.
+   */
+  params?: T;
+
+  /**
+   * A set of query parameter overrides.
+   *
+   * This property allows you to selectively modify or update specific query
+   * parameters from the existing set within the URL. Any keys specified in this
+   * object will take precedence over their corresponding values from the `params`
+   * property.
+   *
+   * If any key is given `null` or `undefined`, it will be removed from the URL.
+   */
+  overrides?: {
+    [K in keyof T]?: T[K];
+  };
+};
+
+/**
+ * Constructs a URL based on the provided route and specified query parameters.
+ *
+ * This function generates a URL string by combining a route with the existing
+ * query parameters and any specified overrides to those parameters.
+ *
+ * @template T The structure of query parameters.
+ * @template Path The path for which to construct the URL.
+ * @returns The constructed URL string.
+ *
+ * @example
+ *
+ * First, let's define a Zod schema for the query parameters.
+ *
+ * ```ts
+ * import { z } from 'zod';
+ *
+ * const Schema = z.object({
+ *   q: z.string().nullish().default(null),
+ *   page: z.number().nullish().default(null),
+ * });
+ *
+ * export type Schema = z.infer<typeof Schema>;
+ * ```
+ *
+ * Using this schema, we can then parse the query parameters from the URL.
+ *
+ * `page.tsx`
+ *
+ * ```tsx
+ * import type { PageProps } from '@/types';
+ *
+ * export default function Page({ searchParams }: PageProps) {
+ *   const parsedParams = Schema.parse(searchParams);
+ *
+ *   return (
+ *     // ...
+ *   )
+ * }
+ * ```
+ *
+ * As an example, let's say we're at the following route:
+ * `/search?q=mario&page=2`. If we want to construct a URL for page 3, we can do
+ * the following:
+ *
+ * ```ts
+ * const url = constructUrl<Schema>({
+ *  route: '/search',
+ *  params: parsedParams,
+ *  overrides: {
+ *    page: 3,
+ *  },
+ * });
+ * ```
+ *
+ * Which would result in the following URL:
+ *
+ * ```
+ * /search?q=mario&page=3
+ * ```
+ *
+ * If we want to remove the `page` parameter from the URL, we can do the
+ * following:
+ *
+ * ```ts
+ * const url = constructUrl<Schema>({
+ *   route: '/search',
+ *   params: parsedParams,
+ *   overrides: {
+ *     page: null,
+ *   },
+ * });
+ *
+ * // => /search?q=mario
+ * ```
+ */
+export function constructUrl<T extends ConstrainedRecord<T>, Path extends string = string>({
+  route,
+  params,
+  overrides,
+}: ConstructUrlOptions<T, Path>): Route<Path> {
+  if (!params) {
+    return route;
+  }
+
+  const query = new URLSearchParams();
+
+  for (const key in params) {
+    const value = overrides && key in overrides ? overrides[key] : params[key];
+
+    if (!Array.isArray(value) && value) {
+      query.append(key, String(value));
+    } else if (Array.isArray(value)) {
+      for (const item of value) {
+        if (item) {
+          query.append(key, String(item));
+        }
+      }
+    }
+  }
+
+  return query.size > 0 ? `${route}?${query.toString()}` : route;
 }
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -6,3 +6,49 @@ export interface RouteItem {
   icon: LucideIcon;
   path: Route;
 }
+
+/**
+ * Represents a primitive value.
+ */
+export type Primitive = string | number | boolean | null;
+
+/**
+ * Ensures that a record contains values constrained to a specific type.
+ *
+ * This type takes in a record and ensures that all values adhers to the
+ * specified type.
+ *
+ * @template T The type of the record to constrain.
+ * @template V The type to constrain the values to.
+ *
+ * @example
+ *
+ * As an example, we'll declare a function that only accepts records with string
+ * values.
+ *
+ * ```ts
+ * declare function onlyStringsSchema<T extends ConstrainedRecord<T, string>>(): void;
+ * ```
+ *
+ * If we create a schema with any non-string values, the function will throw an
+ * error.
+ *
+ * ```ts
+ * type Schema = {
+ *   a: string;
+ *   b: number;
+ * }
+ *
+ * onlyStringsSchema<Schema>(); // Error: Type 'number' is not assignable to type 'string'.
+ *
+ * type Schema = {
+ *   a: string;
+ *   b: string;
+ * }
+ *
+ * onlyStringsSchema<Schema>(); // OK
+ * ```
+ */
+export type ConstrainedRecord<T extends Record<string, unknown>, V = Primitive | Primitive[]> = {
+  [K in keyof T]: T[K] extends V ? T[K] : never;
+};
