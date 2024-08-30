ac58d114e9e7b4adc48aaec604b80d3368054b0a
Norviah <21983700+Norviah@users.noreply.github.com>
2024-08-29 22:05:32 -0700

    feat(types): add a type to ensure a subset of keys are required

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -88,3 +88,40 @@ export type ConstrainedRecord<T extends Record<string, unknown>, V = Primitive |
 export type Explicit<T> = {
   [K in keyof Required<T>]: T[K];
 };
+
+/**
+ * Ensures that the specified subset of keys in an interface are non-nullable.
+ *
+ * This type merges an interface with a subset of its keys, marking these
+ * properties as required and non-nullable, ensuring that they are provided.
+ *
+ * @template T The interface to merge.
+ * @template K The subset of keys to mark as required and non-nullable.
+ *
+ * @example
+ *
+ * ```ts
+ * interface Person {
+ *   name: string;
+ *   job?: string;
+ * }
+ *
+ * declare function greet(person: With<Person, 'job'>): void;
+ *
+ * const unemployedPerson: Person = {
+ *   name: 'John Doe'
+ * };
+ *
+ * greet(unemployedPerson); // Error: `job` is required
+ *
+ * const employedPerson: With<Person, 'job'> = {
+ *   name: 'John Doe',
+ *   job: 'Software Engineer'
+ * };
+ *
+ * greet(employedPerson); // OK
+ * ```
+ */
+export type With<T, K extends keyof T> = T & {
+  [P in keyof Explicit<T>]: P extends K ? NonNullable<T[P]> : T[P];
+};
