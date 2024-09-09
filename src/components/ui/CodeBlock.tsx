beca74d41088fafc33e43d6424b5f467d8a6f8c8
Norviah <21983700+Norviah@users.noreply.github.com>
2024-09-09 15:28:59 -0700

    feat(components/CodeBlock): add an option to add a copy button

--- src/components/ui/CodeBlock.tsx
+++ src/components/ui/CodeBlock.tsx
@@ -1,17 +1,41 @@
+import { CopyContentButton } from '@/components/CopyContentButton';
+
 import { cn } from '@/lib/utils';
 
-export type CodeBlockProps = JSX.IntrinsicElements['code'];
+import type { CopyContentButtonProps } from '@/components/CopyContentButton';
+import type { RequireAllOrNone } from 'type-fest';
 
-export function CodeBlock(props: CodeBlockProps): JSX.Element {
+type DivProps = JSX.IntrinsicElements['div'];
+
+type CodeBlockBaseProps = {
+  /**
+   * Whether to display a copy button for the code block.
+   */
+  copy?: boolean;
+} & CopyContentButtonProps;
+
+export type CodeBlockProps = DivProps & RequireAllOrNone<CodeBlockBaseProps, 'copy' | 'message'>;
+
+export function CodeBlock({
+  copy,
+  className,
+  message,
+  prompt,
+  duration,
+  ...props
+}: CodeBlockProps): JSX.Element {
   return (
-    <code
-      {...props}
+    <div
       className={cn(
-        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono font-semibold text-sm',
-        props.className,
+        'rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono font-semibold text-sm',
+        copy && 'relative',
+        className,
       )}
+      {...props}
     >
       {props.children}
-    </code>
+
+      {copy && <CopyContentButton message={message} prompt={prompt} duration={duration} />}
+    </div>
   );
 }
