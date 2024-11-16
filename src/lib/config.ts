1bbd293a84dc4c17c9bbc13867c95ba0c5447616
Norviah <21983700+Norviah@users.noreply.github.com>
2024-11-15 17:16:56 -0800

    feat(config): simplify the app's description

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,8 +2,7 @@ import { Overpass } from 'next/font/google';
 
 export const siteConfig = {
   name: 'Media Hub',
-  description:
-    'A web application that provides a place to create various collections of your favorite media.',
+  description: 'A web application for creating collections of your favorite media.',
   url: 'https://mediahub.vercel.app',
   author: {
     name: 'Norviah',
