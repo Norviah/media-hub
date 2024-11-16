0032b63ad3236625d161d698200633d138b4c51c
Norviah <21983700+Norviah@users.noreply.github.com>
2024-11-16 14:54:02 -0800

    style(components/Input): set the border color to `border`

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'peer/input group box-border block w-full rounded-md border border-control bg-card px-3 py-2 text-card-foreground text-sm leading-4 placeholder-card-foreground-light shadow-sm outline-none transition-all focus:ring-2 focus:ring-current focus-visible:border-foreground-muted focus-visible:shadow-md focus-visible:ring-background-control',
+          'peer/input group box-border block w-full rounded-md border border-border bg-card px-3 py-2 text-card-foreground text-sm leading-4 placeholder-card-foreground-light shadow-sm outline-none transition-all focus:ring-2 focus:ring-current focus-visible:border-foreground-muted focus-visible:shadow-md focus-visible:ring-background-control',
           className,
         )}
         ref={ref}
