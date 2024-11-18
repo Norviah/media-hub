fc3b0381adbea9ab8b616d9ab712afd72f63593b
Norviah <21983700+Norviah@users.noreply.github.com>
2024-11-18 06:50:55 -0800

    style(ToastProvider): update styling for toast notifications
    - use custom status icons with color
    - change the text color for titles and description
    - provide a custom className function

--- src/components/ToastProvider.tsx
+++ src/components/ToastProvider.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { CircleAlertIcon, CircleCheckIcon, InfoIcon } from 'lucide-react';
+import { Toaster as BaseToastProvider } from 'sonner';
+
 import { cn } from '@/lib/utils';
 import { useTheme } from 'next-themes';
-import { Toaster as BaseToastProvider } from 'sonner';
 
 type ToasterProps = React.ComponentProps<typeof BaseToastProvider>;
 
@@ -14,15 +16,20 @@ export function ToastProvider({ className, ...props }: ToasterProps): JSX.Elemen
       theme={theme as ToasterProps['theme']}
       className={cn('toaster group', className)}
       position='bottom-left'
+      cn={cn}
       toastOptions={{
         classNames: {
-          toast:
-            'group toast group-[.toaster]:bg-card group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg font-normal',
-          title: 'group-[.toast]:font-semibold',
-          description: 'group-[.toast]:text-foreground-light',
-          actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+          toast: 'group toast bg-card text-foreground-dark border-border shadow-lg font-normal',
+          title: 'font-semibold text-foreground-dark',
+          description: 'text-foreground',
           cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
         },
+      }}
+      icons={{
+        info: <InfoIcon className='fill-nord-blue text-background' />,
+        warning: <CircleAlertIcon className='fill-nord-yellow text-background' />,
+        error: <CircleAlertIcon className='fill-nord-red text-background' />,
+        success: <CircleCheckIcon className='fill-nord-green text-background' />,
       }}
       {...props}
     />
